745b0685b7d16035ac0a5aac03fedf78b9a936aa
mmartin4972 <mmartin4972@gmail.com>
2020-12-18 23:33:49 -0500

    Minor debug modifications

---
+++ jetson/percep/builddir/config.h
@@ -0,0 +1,12 @@
+#pragma once
+#define AR_DETECTION 0
+#define AR_RECORD 0
+#define OBSTACLE_DETECTION 1
+/* #undef OBS_RECORD */
+#define ZED_SDK_PRESENT 1
+#define PERCEPTION_DEBUG 1
+#define WRITE_CURR_FRAME_TO_DISK 0
+#define DEFAULT_ONLINE_DATA_FOLDER "/home/jessica/auton_data/"
+#define GPU 0
+
+
